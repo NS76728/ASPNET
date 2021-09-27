332207857192fcd26dc19da04f27c46a623c0aed
Nick Stockton <phoenix76728@gmail.com>
2021-09-27 15:50:31 -0400

    Part V done

---
+++ Testing/Models/IProductRepo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Testing.Models
+{
+    public interface IProductRepo
+    {
+        public IEnumerable<Product> GetAllProducts();
+        public Product GetProduct(int id);
+        void UpdateProduct(Product product);
+        public void InsertProduct(Product productToInsert);
+        public IEnumerable<Category> GetCategories();
+        public Product AssignCategory();
+        public void DeleteProduct(Product product);
+
+
+
+
+    }
+
+
+}
